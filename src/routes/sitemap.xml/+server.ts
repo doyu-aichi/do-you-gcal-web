34a7f6ea0fba8f006747bdf3117d7a9573d363c9
masatoshi ishida <baikuken@gmail.com>
2023-02-25 19:33:05 +0900

    sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,35 @@
+export const GET = async () => {
+  return new Response(
+    `
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+        <url>
+          <loc>https://do-you-gcal.netlify.app/</loc>
+          <lastmod>2023-02-25</lastmod>
+          <priority>1.0</priority>
+        </url>
+        <url>
+          <loc>https://do-you-gcal.netlify.app/privacy</loc>
+          <lastmod>2023-02-25</lastmod>
+          <priority>0.8</priority>
+        </url>
+        <url>
+          <loc>https://do-you-gcal.netlify.app/support</loc>
+          <lastmod>2023-02-25</lastmod>
+          <priority>0.8</priority>
+        </url>
+      </urlset>`.trim(),
+    {
+      headers: {
+        'Content-Type': 'application/xml'
+      }
+    }
+  );
+}
