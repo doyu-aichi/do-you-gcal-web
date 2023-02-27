a46f81e36df8b6ea5f9cc08d644d9d229a7d827d
masatoshi ishida <baikuken@gmail.com>
2023-02-27 10:00:30 +0900

    prerencer

--- svelte.config.js
+++ svelte.config.js
@@ -17,9 +17,8 @@ const config = {
 		}),
 		prerender: {
 			crawl: true,
-			enabled: true,
-			onError: 'continue',
-			pages: ['*'],
+			entries: ['*'],
+			handleHttpError: 'continue',
 		},
 	},
 	outDir: "build"
