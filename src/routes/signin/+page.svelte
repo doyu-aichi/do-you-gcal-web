1ca25ac5e55bd073e7c8b2bc848939916c7b4e21
masatoshi ishida <baikuken@gmail.com>
2023-03-09 12:21:37 +0900

    mild

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<div id="onecup">
+	<div style="margin-top:100px;text-align:center">
+		<div style="margin:60px 0px 50px 0px;font-size:30px">
+			Sign in to Do You Gcal あいち同友会カレンダー同期
+		</div>
+		<div style="position:relative;width:300px;margin:0px auto">
+			<button class="btn google-button" onclick="window._handler[0].apply(null, arguments);"
+				><img
+					class="store"
+					src="/img/oauth/signIn_google.png"
+					width="261px"
+					height="47px"
+					alt="Sign up with Google"
+				/></button
+			>
+		</div>
+	</div>
+</div>
